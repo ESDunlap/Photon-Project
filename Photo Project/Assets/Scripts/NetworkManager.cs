1170840ffc15002c6f376a02600f862caaba2d6d
ESDunlap <dunlape3@my.erau.edu>
2025-08-29 10:58:54 -0700

    Update

--- Photo Project/Assets/Scripts/NetworkManager.cs
+++ Photo Project/Assets/Scripts/NetworkManager.cs
@@ -1,6 +1,48 @@
 using UnityEngine;
+using Photon.Pun;
 
-public class NetworkManager
+public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    
+    public static NetworkManager instance;
+
+    public override void OnConnectedToMaster()
+    {
+        CreateRoom("testroom");
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created room: " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    void Awake()
+    {
+        // if an instance already exists and it's not this one - destroy us
+        if (instance != null && instance != this)
+            gameObject.SetActive(false);
+        else
+        {
+            // set the instance
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void CreateRoom(string roomName)
+    {
+        PhotonNetwork.CreateRoom(roomName);
+    }
+
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+    public void ChangeScene(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
 }
