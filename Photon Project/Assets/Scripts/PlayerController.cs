eb24a29fa9caad2cda08974a6c19b794f2320d21
ESDunlap <dunlape3@my.erau.edu>
2025-09-22 10:25:40 -0700

    Fixed Game Name and Build

---
+++ Photon Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,116 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Unity.VisualScripting;
+
+public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
+{
+    [HideInInspector]
+    public int id;
+    [Header("Info")]
+    public float moveSpeed;
+    public float jumpForce;
+    public GameObject hatObject;
+    [HideInInspector]
+    public float curHatTime;
+    [Header("Components")]
+    public Rigidbody rig;
+    public Player photonPlayer;
+
+    void Update()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            if (curHatTime >= GameManager.instance.timeToWin && !GameManager.instance.gameEnded)
+            {
+                GameManager.instance.gameEnded = true;
+                GameManager.instance.photonView.RPC("WinGame", RpcTarget.All, id);
+            }
+        }
+        if (photonView.IsMine)
+            if(Input.GetKeyUp(KeyCode.Space))
+                TryJump();
+    }
+
+    void FixedUpdate()
+    {
+        if (photonView.IsMine)
+        {
+            Move();
+            // track the amount of time we're wearing the hat
+            if (hatObject.activeInHierarchy)
+                curHatTime += Time.deltaTime;
+        }
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+        //rig.linearVelocity = new Vector3(x, rig.linearVelocity.y, z);
+        rig.AddForce(transform.right * -z);
+        rig.AddForce(transform.forward * x);
+    }
+
+    void TryJump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        if (Physics.Raycast(ray, 0.7f))
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+    [PunRPC]
+    public void Initialize(Player player)
+    {
+        photonPlayer = player;
+        id = player.ActorNumber;
+        GameManager.instance.players[id - 1] = this;
+        // give the first player the hat
+        // if this isn't our local player, disable physics as that's
+        // controlled by the user and synced to all other clients
+        if (!photonView.IsMine)
+            rig.isKinematic = true;
+        // give the first player the hat
+        if (id == 1)
+            GameManager.instance.GiveHat(id, true);
+    }
+    public void SetHat(bool hasHat)
+    {
+        hatObject.SetActive(hasHat);
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (!photonView.IsMine)
+            return;
+        // did we hit another player?
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // do they have the hat?
+            if (GameManager.instance.GetPlayer(collision.gameObject).id == GameManager.instance.playerWithHat)
+            {
+                // can we get the hat?
+                if (GameManager.instance.CanGetHat())
+                {
+                    // give us the hat
+                    GameManager.instance.photonView.RPC("GiveHat", RpcTarget.All, id, false);
+                }
+            }
+        }
+        if (collision.gameObject.CompareTag("SpeedUp"))
+        {
+            GameManager.instance.photonView.RPC("SlowDown", RpcTarget.All, id);
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(curHatTime);
+        }
+        else if (stream.IsReading)
+        {
+            curHatTime = (float)stream.ReceiveNext();
+        }
+    }
+}
